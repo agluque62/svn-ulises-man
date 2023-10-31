83320ba092c12b9dd7b2b1bf69ca67c2327eca0f
Arturo Garcia <arturo.garcia.luque@gmail.com>
2023-10-31 15:55:40 +0100

    Comienzo de la clase de simulación del campo de equipos externos...

---
+++ Pruebas/XProccessUnitTests/MockedClasss/MockedExtEq.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Moq;
+
+using U5kManServer;
+using U5kManServer.Procesos;
+
+
+namespace XProccessUnitTests.MockedClasss
+{
+    public class MockedExtEq
+    {
+        public IProcessData ProcessData => ProcessDataMock.Object;
+        public IProcessPing ProcessPing => ProcessPingMock.Object;
+        public IProcessSip ProcessSip => ProcessSipMock.Object;
+
+        public MockedExtEq() 
+        {
+            ProcessDataMock = new Mock<IProcessData>();
+            ProcessPingMock = new Mock<IProcessPing>();
+            ProcessSipMock = new Mock<IProcessSip>();
+        }
+        void SetupProcessData()
+        {
+            ProcessDataMock.Setup(library => library.Data)
+                .Returns(mockedData);
+            ProcessDataMock.Setup(x => x.IsMaster)
+                .Returns(true);
+        }
+        void SetupProcessPing()
+        {
+
+        }
+        void SetupProcessSip()
+        {
+
+        }
+
+        U5kManStdData mockedData => new U5kManStdData()
+        {
+        };
+        readonly Mock<IProcessData> ProcessDataMock;
+        readonly Mock<IProcessPing> ProcessPingMock;
+        readonly Mock<IProcessSip> ProcessSipMock;
+    }
+}
